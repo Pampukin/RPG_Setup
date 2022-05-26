db139b31cb0b0cf24c2c14a57235dfcc36575f40
Pampukin <20010903yuusuke@gmail.com>
2022-05-27 03:56:22 +0900

    Unity14

---
+++ Assets/Scene/PlayBGM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayBGM : MonoBehaviour
+{
+    [SerializeField] AudioClip BGM = null;//再生させる音楽であるAudioClipとの連携のために導入
+    AudioSource Audio;//音楽を再生させるためにAudioSourceコンポーネントのPlayメソッドを使うために定義する
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();//このスクリプトがアタッチされたオブジェクトに付与されているAudioSourceコンポーネントを認識させる
+        Audio.PlayOneShot(BGM);//変数BGMが表す音楽を再生する
+    }
+}
